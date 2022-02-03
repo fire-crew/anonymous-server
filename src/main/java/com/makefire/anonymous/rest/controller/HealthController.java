07a9af175c219cf9be1200290f52c740d0614384
choipureum <pooreumsunny@gmail.com>
2022-02-03 22:07:02 +0900

    Add swagger config
    Signed-off-by: choipureum <pooreumsunny@gmail.com>

--- src/main/java/com/makefire/anonymous/rest/controller/HealthController.java
+++ src/main/java/com/makefire/anonymous/rest/controller/HealthController.java
@@ -1,7 +1,11 @@
 package com.makefire.anonymous.rest.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  *packageName : com.makefire.anonymous
@@ -14,9 +18,16 @@ import org.springframework.web.bind.annotation.GetMapping;
  * 22-01-15 최푸름
  * ---------------------------------
  */
-@Controller
+@RestController
 public class HealthController {
 
+    @Operation(summary = "test hello", description = "hello api example")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK !!"),
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST !!"),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND !!"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR !!")
+    })
     @GetMapping("/healthCheck")
     public String healthCheck(){
         return "health Checking...";
