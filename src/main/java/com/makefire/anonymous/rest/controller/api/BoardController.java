4adc451dc6a9a25f9efe0f8d955dffffad7c8633
chojiwon <gwon188@gmail.com>
2022-02-08 23:17:00 +0900

    board CRUD,find

--- src/main/java/com/makefire/anonymous/rest/controller/api/BoardController.java
+++ src/main/java/com/makefire/anonymous/rest/controller/api/BoardController.java
@@ -1,10 +1,9 @@
 package com.makefire.anonymous.rest.controller.api;
 
 import com.makefire.anonymous.domain.board.repository.BoardRepository;
+import com.makefire.anonymous.domain.board.repository.SpringDataBoardRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -12,9 +11,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class BoardController {
 
     private final BoardRepository boardRepository;
+    private final SpringDataBoardRepository springDataBoardRepository;
 
     @GetMapping("search")
     public String searchAllBoard() {
         return boardRepository.findAll().toString();
     }
+
+    @GetMapping("find")
+    public String searchByBoardName(@RequestParam(value = "name") String boardName) {
+        return boardRepository.findByBoardName(boardName).toString();
+    }
+
+
+
+//    @GetMapping("searchParamRepo")
+//    public String searchParamRepoMember(@RequestParam(value = "name") String boardName) {
+//        return boardRepository.searchParamRepo(boardName).toString();
+//    }
 }
