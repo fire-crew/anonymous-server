5097be2b19e1e72634f92ef2540be44fedd74e34
chojiwon <gwon188@gmail.com>
2022-03-18 00:40:58 +0900

    controllerTest,selectBoardTest,serviceTest

--- src/test/java/com/makefire/anonymous/support/fixture/BoardFixture.java
+++ src/test/java/com/makefire/anonymous/support/fixture/BoardFixture.java
@@ -2,11 +2,22 @@ package com.makefire.anonymous.support.fixture;
 
 
 import com.makefire.anonymous.rest.dto.request.board.BoardRequest;
+import com.makefire.anonymous.rest.dto.response.board.BoardResponse;
 
 public class BoardFixture {
 
     public static BoardRequest createBoardRequest(){
         return BoardRequest.builder()
+                .id(1L)
+                .title("testTitle")
+                .content("testContent")
+                .author("testAuthor")
+                .authorId(12L)
+                .build();
+    }
+
+    public static BoardResponse createBoardResponse(){
+        return BoardResponse.builder()
                 .id(1L)
                 .title("testTitle")
                 .content("testContent")
