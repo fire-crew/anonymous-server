dc38be0c138c91fa32a160e94cd05180986487d6
강정호 <jhkang@gangjeonghoui-MacBookPro.local>
2022-03-26 10:45:27 +0900

    :recycle: feature: Add @Nested Annotation
    Signed-off-by: 강정호 <jhkang@gangjeonghoui-MacBookPro.local>

--- src/test/java/com/makefire/anonymous/service/board/BoardServiceTest.java
+++ src/test/java/com/makefire/anonymous/service/board/BoardServiceTest.java
@@ -6,10 +6,7 @@ import com.makefire.anonymous.rest.dto.request.board.BoardRequest;
 import com.makefire.anonymous.rest.dto.response.board.BoardResponse;
 import com.makefire.anonymous.support.ServiceTestSupport;
 import com.makefire.anonymous.support.fixture.BoardFixture;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
@@ -39,65 +36,58 @@ public class BoardServiceTest extends ServiceTestSupport {
     @InjectMocks
     private BoardService boardService;
 
-    @BeforeEach
-    void setUp() {
-    }
-
-    @Test
-    @DisplayName("insertBoardTest")
-    void insertBoardTest() {
+    @Nested
+    class CreateBoardRequest {
         BoardRequest boardRequest = BoardFixture.createOneBoardRequest();
-        when(boardRepository.save(any())).thenReturn(BoardRequest.to(boardRequest));
 
-        BoardResponse boardResponse = boardService.insertBoard(boardRequest);
+        @Test
+        @DisplayName("입력받은 boardRequest 데이터를 Repository 에 저장한다.")
+        void insertBoardTest() {
+            when(boardRepository.save(any())).thenReturn(BoardRequest.to(boardRequest));
+            BoardResponse boardResponse = boardService.insertBoard(boardRequest);
 
-        Assertions.assertAll(
-                () -> assertEquals(boardRequest.getTitle(), boardResponse.getTitle())
-        );
+            Assertions.assertAll(
+                    () -> assertEquals(boardRequest.getTitle(), boardResponse.getTitle())
+            );
+        }
+
+        @Test
+        @DisplayName("Repository 에 저장되어 있는 기존 데이터를 입력받은 boardRequest 데이터로 업데이트한다.")
+        void updateBoardTest() {
+            BoardRequest boardRequest1 = BoardFixture.createOneUpdateBoardRequest();
+            when(boardRepository.findById(any())).thenReturn(Optional.of(BoardRequest.to(boardRequest)));
+            BoardResponse boardResponse = boardService.updateBoard(boardRequest1);
+
+            Assertions.assertAll(
+                    () -> assertEquals(boardRequest1.getTitle(), boardResponse.getTitle())
+            );
+        }
+
+        @Test
+        @DisplayName("입력받은 id 로 Repository 에 저장되어 있는 데이터를 찾아 반환한다.")
+        void selectBoardTest() {
+            when(boardRepository.findById(any())).thenReturn(Optional.of(BoardRequest.to(boardRequest)));
+            BoardResponse boardResponse = boardService.selectBoard(1L);
+
+            Assertions.assertAll(
+                    () -> assertEquals(boardRequest.getTitle(), boardResponse.getTitle())
+            );
+        }
+
+        @Test
+        @DisplayName("입력받은 id 로 Repository 에 저장되어 있는 데이터를 삭제한다.")
+        void deleteBoardTest() {
+            when(boardRepository.findById(any())).thenReturn(Optional.of(BoardRequest.to(boardRequest)));
+            Boolean result = boardService.deleteBoard(1L);
+
+            Assertions.assertAll(
+                    () -> assertEquals(true, result)
+            );
+        }
     }
 
     @Test
-    @DisplayName("updateBoardTest")
-    void updateBoardTest() {
-        BoardRequest boardRequest = BoardFixture.createOneBoardRequest();
-        BoardRequest boardRequest1 = BoardFixture.createOneUpdateBoardRequest();
-        when(boardRepository.findById(any())).thenReturn(Optional.of(BoardRequest.to(boardRequest)));
-
-        BoardResponse boardResponse = boardService.updateBoard(boardRequest1);
-
-        Assertions.assertAll(
-                () -> assertEquals(boardRequest1.getTitle(), boardResponse.getTitle())
-        );
-    }
-
-    @Test
-    @DisplayName("selectBoardTest")
-    void selectBoardTest() {
-        BoardRequest boardRequest = BoardFixture.createOneBoardRequest();
-        when(boardRepository.findById(any())).thenReturn(Optional.of(BoardRequest.to(boardRequest)));
-
-        BoardResponse boardResponse = boardService.selectBoard(1L);
-
-        Assertions.assertAll(
-                () -> assertEquals(boardRequest.getTitle(), boardResponse.getTitle())
-        );
-    }
-
-    @Test
-    @DisplayName("deleteBoardTest")
-    void deleteBoardTest() {
-        BoardRequest boardRequest = BoardFixture.createOneBoardRequest();
-        when(boardRepository.findById(any())).thenReturn(Optional.of(BoardRequest.to(boardRequest)));
-
-        Boolean result = boardService.deleteBoard(1L);
-
-        Assertions.assertAll(
-                () -> assertEquals(true, result)
-        );
-    }
-
-    @Test
-    @DisplayName("selectBoardListTest")
+    @DisplayName("Repository 에 저장되어 있는 모든 데이터를 찾아 반환한다.")
     void selectBoardListTest() {
         List<Board> boardList = BoardFixture.createSeveralBoard();
         when(boardRepository.findAll()).thenReturn(boardList);
